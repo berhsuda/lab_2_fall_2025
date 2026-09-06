8e1583a5d70f3ff8bcd87df32be7530b6b08e785
David Hsu <berhsuda@gmail.com>
2026-09-05 23:28:42 -0700

    kinematic chain

--- lab_2.py
+++ lab_2.py
@@ -95,20 +95,28 @@ class ForwardKinematics(Node):
 
         # T_1_2 (leg_front_l_1 to leg_front_l_2)
         ## TODO: Implement the transformation matrix from leg_front_l_1 to leg_front_l_2
-        T_1_2 = None
+        # translate on z axis 
+        translate_1 = translation(0,0,-0.039)
+        orient_2 = rotation_y(-1.57080)
+        motor_2 = rotation_z(theta2)
+        T_1_2 = translate_1 @ orient_2 @ motor_2
 
         # T_2_3 (leg_front_l_2 to leg_front_l_3)
         ## TODO: Implement the transformation matrix from leg_front_l_2 to leg_front_l_3
-        T_2_3 = None
+        translate_2 = translation(0,-0.0494,0.0685)
+        orient_3 = rotation_y(1.57080)
+        motor_3 = rotation_z(theta3)
+        T_2_3 = translate_2 @ orient_3 @ motor_3
 
         # T_3_ee (leg_front_l_3 to end-effector)
-        T_3_ee = None
+        
+        T_3_ee = translation(0.06231,-0.06216,-0.018)
 
         # TODO: Compute the final transformation. T_0_ee is the multiplication of the previous transformation matrices
-        T_0_ee = None
+        T_0_ee = T_0_1 @ T_1_2 @ T_2_3 @ T_3_ee
 
         # TODO: Extract the end-effector position. The end effector position is a 3x1 vector (not in homogenous coordinates)
-        end_effector_position = None
+        end_effector_position = T_0_ee
 
         return end_effector_position
 
